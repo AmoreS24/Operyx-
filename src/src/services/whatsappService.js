57efef5413f078ac2d1c380c9b5fe9242b1a1a09
AmoreS24 <erickoliveira443@gmail.com>
2026-04-14 10:48:43 -0300

    Criar whatsappService.js

---
+++ src/src/services/whatsappService.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+
+const TOKEN = 'EAAhjp9ko1ZB4BRMTvZAa9TAUQVqJG93IUBZC7DZCtdZAmC2Blvnogtc5OZBKvhM6PqTZCudWXYvx0zM2r2ANR0tYQoqE4NxL8Qyibk7y9AjWlcByZAga5mDrMV7xukqzM2h9mUGTeqAlb6CAwjD82KDNOTCYD3UiZC9U6w9wAJSgQYEettV4nSq8yHxmLQGTreiayAcFZBlzY3nZBrZB683xrAeT7mBIrRj9ZBu7hOGUXZCUXiEUZBmZCFoPXTjxIeHRODNZAYEdzvroL4YAOYV742K0ZAh9lKsNUbuBM5s1oGsX4ZD';
+const PHONE_NUMBER_ID = '1061662853703569';
+
+async function enviarMensagem(numero, mensagem) {
+  try {
+    const response = await axios.post(
+      `https://graph.facebook.com/v18.0/${PHONE_NUMBER_ID}/messages`,
+      {
+        messaging_product: "whatsapp",
+        to: numero,
+        type: "text",
+        text: {
+          body: mensagem
+        }
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+
+    console.log('Mensagem enviada:', response.data);
+  } catch (error) {
+    console.error('Erro ao enviar mensagem:', error.response?.data || error.message);
+  }
+}
+
+module.exports = { enviarMensagem };
