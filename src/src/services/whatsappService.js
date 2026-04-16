fc2c1074742e23e0f86df3a2dd815f555b1477b9
AmoreS24 <erickoliveira443@gmail.com>
2026-04-16 19:56:21 -0300

    Atualizar o whatsappService.js

--- src/src/services/whatsappService.js
+++ src/src/services/whatsappService.js
@@ -1,34 +1,29 @@
-const axios = require('axios');
+const axios = require("axios");
 
-const TOKEN = 'EAAhjp9ko1ZB4BRBI0JKBfjESbJO92cT6FJnTFH8goyQQ8EeP7fvP3S3ERb9UztI3wmcZCSQ1YZAYBEGEMSUrs6yO4eLQQJl3ZBgGjbUHBDdE2p4Ic44LMixUalFCISher1FvjZAbET5TBe8OIE5zWeZCP25U2pzjeVsQ7aFWHhaorMGqomZCKjrlXC5Rbhh7l3EhB378dmPkL1WR8bjmaUIpc6qtCpo39tyw7YyZATuVB9wIhlXlZC8A9ka3xNw8IrfEhtzJ1yYgBc3ipfvYyNbB0eB7f9Ocv5VYgL5Ab';
-const PHONE_NUMBER_ID = '1061662853703569';
+const TOKEN = process.env.WHATSAPP_TOKEN || "EAAhjp9ko1ZB4BRBG4stn9FkY4CAZC45fvMxgRMnQ0FLh8DhV6iETsabZCdwP4Kd0J1f54ZBVWlFPL5u5iJeMsoJ5byy6AGWfjzMUlFluE6viMhnyQ7JZAZBI0ly4Nyi9R0Ar5sfGKvBqVqFh3Cy0ZAyqkJo8vP3F8tTtz0szAcmcb4gNRvLFtZB4yE1vKXIGsZAvkdd9uJiAgbVjckAlJOKZCG8CSsrcV1WPPol9IN4FD6iKhWzxwbs3BJAEn6lc7Po1t6U0SEY4tj6EIaqufyQRZBbSMLD3tsDmOMZD";
+const PHONE_NUMBER_ID = process.env.WHATSAPP_PHONE_NUMBER_ID || "1061662853703569";
 
 async function enviarMensagem(numero, mensagem) {
-  try {
-    const response = await axios.post(
-      `https://graph.facebook.com/v18.0/${PHONE_NUMBER_ID}/messages`,
-      {
-        messaging_product: 'whatsapp',
-        to: numero,
-        type: 'text',
-        text: {
-          body: mensagem || 'Fala Erick 🚀 agora sim funcionando!'
-        }
+  const response = await axios.post(
+    `https://graph.facebook.com/v18.0/${PHONE_NUMBER_ID}/messages`,
+    {
+      messaging_product: "whatsapp",
+      to: numero,
+      type: "text",
+      text: {
+        body: mensagem,
       },
-      {
-        headers: {
-          Authorization: `Bearer ${TOKEN}`,
-          'Content-Type': 'application/json'
-        }
-      }
-    );
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
 
-    console.log('Mensagem enviada:', response.data);
-    return response.data;
-  } catch (error) {
-    console.error('Erro ao enviar mensagem:', error.response?.data || error.message);
-    throw error;
-  }
+  console.log("Mensagem enviada:", response.data);
+  return response.data;
 }
 
 module.exports = { enviarMensagem };
